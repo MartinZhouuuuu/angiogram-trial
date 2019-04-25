bfc2711f1094e075770b2972bcb24a9e49e24308
Zhou Chengyang <zhoulele12@gmail.com>
2019-04-26 00:08:59 +0800

    changed to categorical crossentropy

--- tell-good-from-bad.py
+++ tell-good-from-bad.py
@@ -6,25 +6,30 @@ from tensorflow.keras.utils import to_categorical
 from imgaug import augmenters as iaa
 model = Sequential()
 #convolution
-model.add(Conv2D(32,(5,5), activation = 'relu', 
+model.add(Conv2D(32,(3,3), activation = 'relu', 
 	input_shape = (360,360,1)))
-#model.add(BatchNormalization())
-model.add(Conv2D(32,(5,5), activation = 'relu'))
-#model.add(BatchNormalization())
+model.add(MaxPooling2D((2,2)))
+model.add(Dropout(0.2))
+model.add(Conv2D(32,(3,3), activation = 'relu'))
+model.add(MaxPooling2D((2,2)))
+model.add(Dropout(0.2))
+model.add(Conv2D(64,(3,3),activation = 'relu'))
 model.add(MaxPooling2D((2,2)))
 model.add(Dropout(0.25))
-model.add(Conv2D(64,(5,5),activation = 'relu'))
-#model.add(BatchNormalization())
-model.add(Conv2D(64,(5,5),activation = 'relu'))
-#model.add(BatchNormalization())
+model.add(Conv2D(64,(3,3),activation = 'relu'))
 model.add(MaxPooling2D(2,2))
 model.add(Dropout(0.25))
-#convert 3D tensor to 1D
 model.add(Flatten())
-model.add(Dense(256,activation = 'relu'))
+'''model.add(Dense(128,activation = 'relu'))
 model.add(Dropout(0.5))
-model.add(Dense(1, activation = 'softmax'))
-model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+model.add(Dense(64,activation = 'relu'))
+model.add(Dropout(0.5))
+model.add(Dense(32,activation = 'relu'))
+model.add(Dropout(0.5))'''
+model.add(Dense(64,activation = 'relu'))
+model.add(Dropout(0.5))
+model.add(Dense(2, activation = 'softmax'))
+model.compile(optimizer = 'rmsprop', loss = 'binary_crossentropy', metrics = ['accuracy'])
 
 train_datagen = ImageDataGenerator(
 	rescale =  1./255,
@@ -39,8 +44,8 @@ train_datagen = ImageDataGenerator(
 train_generator = train_datagen.flow_from_directory(
 	'good-bad',
 	target_size = (360,360),
-	batch_size = 1,
-	class_mode = 'binary',
+	batch_size = 16,
+	class_mode = 'categorical',
 	color_mode = 'grayscale',
 	)
 '''test_datagen = ImageDataGenerator(
@@ -56,7 +61,7 @@ test_generator = test_datagen.flow_from_directory(
 
 model.fit_generator(
 	train_generator,
-	steps_per_epoch = 600,
+	steps_per_epoch = 6000,
 	epochs = 30,
 	)
 '''validation_data = test_generator,
