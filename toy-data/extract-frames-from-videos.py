ee6a3a836d906ef6a7c83c24000cd20b814c1625
Zhou Chengyang <zhoulele12@gmail.com>
2019-04-25 09:59:58 +0800

    extracted all images

--- toy-data/extract-frames-from-videos.py
+++ toy-data/extract-frames-from-videos.py
@@ -1,13 +1,21 @@
 import cv2 
+import os
 def extract_frames(path):
 	video_object = cv2.VideoCapture(path)
 
 	count = 0
 	success = 1
-
 	while success:
 		success, image = video_object.read()
-
-		cv2.imwrite('frame%d'%count, image)
 		count += 1
-
+		if not os.path.exists(
+			'/Users/apple/Google Drive/angiogram-trial/extracted-frames/%s'%path.replace('.mp4','')
+			):
+			os.makedirs('/Users/apple/Google Drive/angiogram-trial/extracted-frames/%s'%path.replace('.mp4',''))
+		cv2.imwrite(
+			'/Users/apple/Google Drive/angiogram-trial/extracted-frames/%s/frame%d.jpg'%(path.replace('.mp4',''),count), image)
+	
+	os.remove(
+		'/Users/apple/Google Drive/angiogram-trial/extracted-frames/%s/frame%d.jpg'%(path.replace('.mp4',''),count)
+		)
+extract_frames('nejm.mp4')
